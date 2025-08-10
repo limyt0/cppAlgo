28fa5557d1e1ad3953b998c114fe6c8368e8e88f
limyt0 <dudx45@gmail.com>
2025-08-10 09:10:56 +0900

    4번 풀이

--- Project1/p004.cpp
+++ Project1/p004.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -8,21 +8,21 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N, M;
+	int N , M;
 	cin >> N >> M;
 	for (int i = 0; i <= N; i++) {
 		for (int j = 0; j <= N; j++) {
 			A[i][j] = 0;
-			if (i != 0 && j != 0) {
+			if (i != 0  && j != 0) {
 				int temp;
 				cin >> temp;
-				A[i][j] = A[i - 1][j] + A[i][j - 1] - A[i - 1][j - 1] + temp;
+				A[i][j] = A[i-1][j] + A[i][j-1] - A[i-1][j-1] + temp;
 			}
 		}
 	}
 
 	for (int i = 0; i < M; i++) {
-		int x1, x2, y1, y2;
+		int x1 , x2 , y1 , y2;
 		cin >> x1 >> y1 >> x2 >> y2;
 
 		int result = A[x2][y2] - A[x1 - 1][y2] - A[x2][y1 - 1] + A[x1 - 1][y1 - 1];
